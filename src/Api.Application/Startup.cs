be38241152b40763a415cc2bc8771b257e7d8e42
marimell09 <mari_carvalhomello@hotmail.com>
2021-11-24 23:19:25 -0300

    Squashed commit of the following:
    commit 6b73c38a62c5fae60359c95311dd2eeb1deffbda
    Author: marimell09 <mari_carvalhomello@hotmail.com>
    Date:   Wed Nov 24 23:16:46 2021 -0300
    
        Finished development for JWT implementation.
    
    commit 3655e9e6e4c816acd345161aee4cdec673d5346f
    Author: marimell09 <mari_carvalhomello@hotmail.com>
    Date:   Wed Nov 24 15:39:32 2021 -0300
    
        Begining JWT and Dto Login implementation.

--- src/Api.Application/Startup.cs
+++ src/Api.Application/Startup.cs
@@ -1,16 +1,15 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Api.CrossCutting.DependencyInjection;
+using Api.Domain.Security;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 
 namespace application
@@ -30,6 +29,36 @@ namespace application
 
             ConfigureService.ConfigureDependenciesService(services);
             ConfigureRepository.ConfigureDependencyRepository(services);
+
+            var signingConfigurations = new SigningConfigurations();
+            services.AddSingleton(signingConfigurations);
+
+            var tokenConfigurations = new TokenConfigurations();
+            new ConfigureFromConfigurationOptions<TokenConfigurations>(
+              Configuration.GetSection("TokenConfigurations"))
+                .Configure(tokenConfigurations);
+            services.AddSingleton(tokenConfigurations);
+
+            services.AddAuthentication(authOptions => {
+              authOptions.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+              authOptions.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(bearerOptions => {
+              var paramsValidation = bearerOptions.TokenValidationParameters;
+              paramsValidation.IssuerSigningKey = signingConfigurations.Key;
+              paramsValidation.ValidAudience = tokenConfigurations.Audience;
+              paramsValidation.ValidIssuer = tokenConfigurations.Issuer;
+              paramsValidation.ValidateIssuerSigningKey = true;
+              paramsValidation.ValidateLifetime = true;
+              paramsValidation.ClockSkew = TimeSpan.Zero;
+            });
+
+            services.AddAuthorization(auth =>
+            {
+              auth.AddPolicy("Bearer", new AuthorizationPolicyBuilder()
+                      .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+                      .RequireAuthenticatedUser().Build());
+            });
+
             services.AddControllers();
             services.AddSwaggerGen(c => {
                 c.SwaggerDoc("v1", new OpenApiInfo
@@ -45,6 +74,26 @@ namespace application
                     License = new OpenApiLicense{
                         Name = "Termo de Licença de Uso"
                     }
+                });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme{
+                  Description = "Entre com o Token JWT",
+                  Name = "Authorization",
+                  In = ParameterLocation.Header,
+                  Type = SecuritySchemeType.ApiKey
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                  {
+                    new OpenApiSecurityScheme
+                    {
+                      Reference = new OpenApiReference
+                      {
+                        Id = "Bearer",
+                        Type = ReferenceType.SecurityScheme
+                      }
+                    }, new List<string>()
+                  }
                 });
             });
         }
