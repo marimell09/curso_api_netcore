53ebad6e1922258b447e007df16f4c139a458af0
marimell09 <mari_carvalhomello@hotmail.com>
2021-11-30 20:32:45 -0300

    Begining implementation for integration tests. Changed token configuration from json inline code to launcher.

---
+++ src/Api.Integration.Test/TesteLogin.cs
@@ -0,0 +1,15 @@
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Api.Integration.Test
+{
+    public class TesteLogin : BaseIntegration
+    {
+
+     [Fact]
+      public async Task TesteDoToken(){
+        await AdicionarToken();
+      }
+
+    }
+}
